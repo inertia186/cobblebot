fc953a5a832ad618b77c742fd5aae043a92f8c74
Anthony Martin <anthony@vasaristudio.com>
2015-03-21 10:42:44 -0700

    order

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
-    @players = Server.players
+    @players = Server.players.order(:last_login_at)
     @players_today = Player.logged_in_today.where.not(id: @players)
   end
 end
