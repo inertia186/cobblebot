bcf5f7ec979fc3d69b495260bf11591e48d75380
Anthony Martin <anthony@vasaristudio.com>
2015-03-25 21:43:29 -0700

    basic player management

--- app/models/server.rb
+++ app/models/server.rb
@@ -9,4 +9,44 @@ class Server
 
     Player.where(nick: nicks.split(", ")).order(:last_login_at)
   end
+  
+  def self.banned_players_file_path
+    raise StandardError.new("Server Path not set.") if ServerProperties.path_to_server.nil?
+    
+    @banned_players_file_path ||= "#{ServerProperties.path_to_server}/banned-players.json"
+  end
+
+  def self.banned_players
+    JSON[File.read banned_players_file_path] if File.exists? banned_players_file_path
+  end
+
+  def self.banned_ips_file_path
+    raise StandardError.new("Server Path not set.") if ServerProperties.path_to_server.nil?
+    
+    @banned_ips_file_path ||= "#{ServerProperties.path_to_server}/banned-ips.json"
+  end
+
+  def self.banned_ips
+    JSON[File.read banned_ips_file_path] if File.exists? banned_ips_file_path
+  end
+
+  def self.ops_file_path
+    raise StandardError.new("Server Path not set.") if ServerProperties.path_to_server.nil?
+    
+    @ops_file_path ||= "#{ServerProperties.path_to_server}/ops.json"
+  end
+
+  def self.ops
+    JSON[File.read ops_file_path] if File.exists? ops_file_path
+  end
+
+  def self.whitelist_file_path
+    raise StandardError.new("Server Path not set.") if ServerProperties.path_to_server.nil?
+    
+    @whitelist_file_path ||= "#{ServerProperties.path_to_server}/whitelist.json"
+  end
+
+  def self.whitelist
+    JSON[File.read whitelist_file_path] if File.exists? whitelist_file_path
+  end
 end
