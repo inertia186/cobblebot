233b8297279f9589d5834382d884c51157924893
Anthony Martin <anthony@vasaristudio.com>
2015-03-27 00:11:06 -0700

    expires

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,10 @@ class ResourcesController < ApplicationController
   def server_icon
     server_icon = Server.server_icon
 
-    return send_data server_icon, type: 'image/png', disposition: 'inline' if !!server_icon
+    if !!server_icon
+      response.headers["Expires"] = CGI.rfc1123_date(3600.seconds.from_now)
+      return send_data server_icon, filename: 'server-icon.png', type: 'image/png', disposition: 'inline'
+    end
     
     render file: "#{Rails.root}/public/404.html", status: 404
   end
