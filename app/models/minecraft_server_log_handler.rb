f64279ddb929cf6a03d10e838e5b875cf337cad3
Anthony Martin <anthony@vasaristudio.com>
2015-04-08 08:12:24 -0700

    Allow the handler to ignore certain types of log entries entirely.

--- app/models/minecraft_server_log_handler.rb
+++ app/models/minecraft_server_log_handler.rb
@@ -1,5 +1,12 @@
 class MinecraftServerLogHandler
+  REGEX_RCON_LISTENER = %r{^\[\d{2}:\d{2}:\d{2}\] \[RCON Listener #[0-9]+/INFO\]: .*$}
+  REGEX_RCON_CLIENT = %r{^\[\d{2}:\d{2}:\d{2}\] \[RCON Client #[0-9]+/INFO\]: .*$}
+  REGEX_NON_LOG_EVENT = %r{^(?!\[\d{2}:\d{2}:\d{2}\]).*$}
+  ALL_REGEX_IGNORE = [REGEX_RCON_LISTENER, REGEX_RCON_CLIENT, REGEX_NON_LOG_EVENT]
+  
   def self.handle(line, options = {})
+    return if Regexp.union(ALL_REGEX_IGNORE).match(line)
+    
     begin
       Rails.logger.info "Handling: #{line}"
       any_result = nil
