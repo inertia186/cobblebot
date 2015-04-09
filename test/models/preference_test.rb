8f5651333d32c89dc99bc02e637a644dd7a19169
Anthony Martin <anthony@vasaristudio.com>
2015-04-09 08:47:21 -0700

    tests

--- test/models/preference_test.rb
+++ test/models/preference_test.rb
@@ -4,6 +4,12 @@ class PreferenceTest < ActiveSupport::TestCase
   def setup
     method = :create!; eval File.read "#{Rails.root}/db/seeds.rb"
   end
+
+  def test_to_param
+    Preference.all.find_each do |p|
+      assert p.to_param, "expect param"
+    end
+  end
   
   def test_web_admin_password
     assert Preference.web_admin_password, "did expect web admin to be set"
@@ -14,6 +20,15 @@ class PreferenceTest < ActiveSupport::TestCase
   end
 
   def test_missing_method
-    refute Preference.method_that_does_not_exist, "did not expect method to exist" rescue return
+    assert Preference.none, "expect method to exist"
+    
+    begin
+      refute Preference.method_that_does_not_exist, 'did not expect method to exist'
+      # :nocov:
+      fail 'did not expect method to exist'
+      # :nocov:
+    rescue ArgumentError => e
+      # success
+    end
   end
 end
