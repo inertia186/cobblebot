6d5cf82bc54c832bd8dba533af334171743a97f8
Anthony Martin <anthony@vasaristudio.com>
2015-04-09 07:42:50 -0700

    tests

--- test/workers/minecraft_server_log_handler_test.rb
+++ test/workers/minecraft_server_log_handler_test.rb
@@ -4,15 +4,39 @@ class MinecraftServerLogHandlerTest < ActiveSupport::TestCase
   def setup
     method = :create!; eval File.read "#{Rails.root}/db/seeds.rb"
     
-    Preference.path_to_server = "#{Rails.root}/tmp"
+    tmp = Preference.path_to_server = "#{Rails.root}/tmp"
+    Dir.mkdir(tmp) unless File.exists?(tmp)
     
-    fake_logs = "#{Preference.path_to_server}/logs"
+    fake_logs = "#{tmp}/logs"
     Dir.mkdir(fake_logs) unless File.exists?(fake_logs)
     fake_latest_log = "#{fake_logs}/latest.log"
     
     File.delete(fake_latest_log) if File.exists?(fake_latest_log)
     File.open(fake_latest_log, 'a') do |f|
-      f << "[15:04:50] [Server thread/INFO]: <inertia186> You try to swing an axe at Shia Labeouf.\n"
+      startup_event = <<-DONE
+        [07:38:13] [Server thread/INFO]: Starting minecraft server version 1.8.3
+        [07:38:13] [Server thread/INFO]: Loading properties
+        [07:38:13] [Server thread/INFO]: Default game type: SURVIVAL
+        [07:38:13] [Server thread/INFO]: Generating keypair
+        [07:38:13] [Server thread/INFO]: Starting Minecraft server on *:25565
+        [07:38:13] [Server thread/INFO]: Using default channel type
+        [07:38:13] [Server thread/INFO]: Preparing level "world"
+        [07:38:14] [Server thread/INFO]: Preparing start region for level 0
+        [07:38:15] [Server thread/INFO]: Preparing spawn area: 24%
+        [07:38:16] [Server thread/INFO]: Preparing spawn area: 74%
+        [07:38:16] [Server thread/INFO]: Done (2.656s)! For help, type "help" or "?"
+        [07:38:16] [Server thread/INFO]: Starting GS4 status listener
+        [07:38:16] [Server thread/INFO]: Starting remote control listener
+        [07:38:16] [Query Listener #1/INFO]: Query running on 0.0.0.0:25565
+        [07:38:16] [RCON Listener #2/INFO]: RCON running on 0.0.0.0:25575
+        [14:58:10] [User Authenticator #3/INFO]: UUID of player inertia186 is d6edf996-6182-4d58-ac1b-4ca0321fb748
+        [14:58:10] [Server thread/INFO]: inertia186[/127.0.0.1:59723] logged in with entity id 44688 at (15.557474447143854, 74.0, 249.30806519239542)
+        [14:58:10] [Server thread/INFO]: inertia186 joined the game
+        [15:04:50] [Server thread/INFO]: <inertia186> You try to swing an axe at Shia Labeouf.
+      DONE
+      startup_event.each_line do |line|
+        f << line.strip + "\n"
+      end
     end
   end
 
