bcc99eea8c7e8a70fcbcb317ed7d60fe3c4fb5ee
Anthony Martin <anthony@vasaristudio.com>
2015-04-10 12:26:36 -0700

    simplify

--- app/concerns/audible.rb
+++ app/concerns/audible.rb
@@ -4,13 +4,20 @@ module Audible
   
   module ClassMethods
     def play_sound(selector, sound, options = {volume: '', pitch: ''})
+      selector = prep_play_sound_selector(selector)    
+      clean_play_sound_result execute("execute #{selector} ~ ~ ~ playsound #{sound} @p ~0 ~0 ~0 #{options[:volume]} #{options[:pitch]}") unless selector.nil?
+    end
+  private
+    def prep_play_sound_selector(selector)
       if (disabled = Server.players.play_sounds(false)).any?
         subs = disabled.map { |p| "name=!#{p.nick}" }
         selector = merge_selectors(selector, "@#{selector[1]}[#{subs.join(',')}]")
       end
-    
-      result = execute("execute #{selector} ~ ~ ~ playsound #{sound} @p ~0 ~0 ~0 #{options[:volume]} #{options[:pitch]}") unless selector.nil?
-    
+      
+      selector
+    end
+  
+    def clean_play_sound_result(result)
       unless result.nil?
         result.split('Played sound ').reject(&:empty?).join(', ').
           split('That player cannot be found').reject(&:empty?).join(', ')
