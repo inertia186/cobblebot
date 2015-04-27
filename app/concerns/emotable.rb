c3b06108279927b7a5354de538134418f4d63c46
Anthony Martin <anthony@vasaristudio.com>
2015-04-26 17:55:42 -0700

    Now it'll work for players that haven't been created yet.

--- app/concerns/emotable.rb
+++ app/concerns/emotable.rb
@@ -22,7 +22,7 @@ module Emotable
       return if Server.players.include?(player)
       
       # Don't do prediction if the player has logged in today.
-      return if !!player.last_login_at && player.last_login_at > 24.hours.ago
+      return if !!player && !!player.last_login_at && player.last_login_at > 24.hours.ago
 
       # Don't do prediction half the time, so it's spooky.
       return if Random.rand(10) % 2 == 0
