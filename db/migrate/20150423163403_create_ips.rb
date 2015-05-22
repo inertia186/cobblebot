cb7e90e50ad67a5fb04f40eb24321924fcce53a8
Anthony Martin <anthony@vasaristudio.com>
2015-05-21 18:40:24 -0700

    whitespace

--- db/migrate/20150423163403_create_ips.rb
+++ db/migrate/20150423163403_create_ips.rb
@@ -1,9 +1,9 @@
 class CreateIps < ActiveRecord::Migration
   def change
     create_table :ips do |t|
-      t.string :address, null:false
+      t.string :address, null: false
       t.integer :player_id, null: false
-      t.string :origin, null:false
+      t.string :origin, null: false
       t.string :created_at, null:false
     end
   end
