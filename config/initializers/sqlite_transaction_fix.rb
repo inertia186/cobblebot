75c98264cfc66c3e696891f7d00deb6a8244b20d
Anthony Martin <anthony@vasaristudio.com>
2015-10-01 12:47:31 -0700

    Made transactions retry.

--- config/initializers/sqlite_transaction_fix.rb
+++ config/initializers/sqlite_transaction_fix.rb
@@ -1,7 +1,19 @@
 module SqliteTransactionFix
-  def begin_db_transaction
-    log('begin immediate transaction', nil) do
-      @connection.transaction(:immediate)
+  def begin_db_transaction(write_timeout = 5000) #:nodoc:
+    if write_timeout.nil?
+      @connection.transaction(:deferred) # Deferred is the default.
+    else
+      deadline = Time.new + (write_timeout.to_f / 1000)
+      success = false
+      while (!success and Time.new() < deadline) do
+        begin
+          @connection.transaction(:immediate)
+          success = true
+        rescue SQLite3::BusyException
+          sleep 0.001
+        end
+      end
+      raise SQLite3::BusyException unless success
     end
   end
 end
