562acc05e584def5c5623fd4c6cbdff1e77c0afe
Anthony Martin <anthony@vasaristudio.com>
2015-10-01 21:56:55 -0700

    debug + exponential backoff

--- config/initializers/sqlite_transaction_fix.rb
+++ config/initializers/sqlite_transaction_fix.rb
@@ -1,19 +1,26 @@
 module SqliteTransactionFix
+  MAX_SLEEP = 30
+  
   def begin_db_transaction(write_timeout = 5000) #:nodoc:
+    @sleep ||= 0.001
+    @sleep = 0.001 if @sleep >= MAX_SLEEP
+    
     if write_timeout.nil?
       @connection.transaction(:deferred) # Deferred is the default.
     else
       deadline = Time.new + (write_timeout.to_f / 1000)
       success = false
+      tries = 0
       while (!success and Time.new() < deadline) do
+        tries = tries + 1
         begin
           @connection.transaction(:immediate)
           success = true
         rescue SQLite3::BusyException
-          sleep 0.001
+          sleep (@sleep = @sleep * 2)
         end
       end
-      raise SQLite3::BusyException unless success
+      raise SQLite3::BusyException("Retries: #{tries}, sleep: #{@sleep}") unless success
     end
   end
 end
