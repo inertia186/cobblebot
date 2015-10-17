58302b9854aac557c7bd435278d3724fe690b6f7
Anthony Martin <anthony@vasaristudio.com>
2015-10-17 14:02:33 -0700

    Fewer player queries.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,7 @@
 class PlayersController < ApplicationController
   def index
     @players = Server.players.with_pvp_counts
-    @players_today = Player.with_pvp_counts.logged_in_today.where.not(id: @players.except(:select))
-    
+
     if params[:after].present?
       head 204 and return if params[:after] == 'undefined'
       
@@ -13,7 +12,10 @@ class PlayersController < ApplicationController
         order('updated_at DESC').map do |p|
           {p.nick => p.last_chat}
         end.reverse
-      head 204 if p.any? && p.activity_after(after).none? && @new_chat.empty?
+        
+      head 204 if p.activity_after(after).none? && @new_chat.empty?
     end
+
+    @players_today = Player.with_pvp_counts.logged_in_today.where.not(id: @players.except(:select))
   end
 end
