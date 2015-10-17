b86bfa762ff970772f63b742924397d2ede68d03
Anthony Martin <anthony@vasaristudio.com>
2015-10-17 10:24:15 -0700

    Storing unique quotes.

--- app/models/message/quote.rb
+++ app/models/message/quote.rb
@@ -1,5 +1,7 @@
 class Message::Quote < Message
   after_initialize :setup_defaults
+
+  validates_uniqueness_of :body, scope: :author, case_sensitive: false
   
   def setup_defaults
     self.recipient_term ||= '@a'
