212c0cd05dc72a2e3f7f4596f2d39f725fc976ce
Anthony Martin <github@martin-studio.com>
2015-11-22 18:13:35 -0800

    Public Donations UI update

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,7 +1,13 @@
 class DonationsController < ApplicationController
+  respond_to :json
+
   def index
     @donations = Message::Donation.order('messages.created_at DESC')
     @donations = @donations.preload(:author)
-    @donations = @donations.paginate(page: params[:page], per_page: 100)
+    
+    respond_to do |format|
+      format.html
+      format.json { respond_with @donations }
+    end
   end
 end
