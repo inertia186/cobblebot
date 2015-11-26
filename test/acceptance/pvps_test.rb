b8a7e25c9aa9924df7f200871b331f9e96ade14f
Anthony Martin <github@martin-studio.com>
2015-11-25 17:13:11 -0800

    Don't mess with other tests.

--- test/acceptance/pvps_test.rb
+++ test/acceptance/pvps_test.rb
@@ -2,10 +2,6 @@ require "test_helper"
 
 class PvpsTest < ActionDispatch::IntegrationTest
   def setup
-    def Server.up?
-      true
-    end
-    
     Message::Pvp.create(body: 'Dinnerbone was shot by Dinnerbone', recipient: players(:Dinnerbone), author: players(:Dinnerbone))
   end
   
@@ -13,5 +9,11 @@ class PvpsTest < ActionDispatch::IntegrationTest
     visit '/pvps'
     fill_in 'query', with: 'dinnerbone'
     save_screenshot
+  end
+end
+
+def Server
+  def self.up?
+    true
   end
 end
