c25cd6d09df7df450c4e460e2b972f60de7c1b56
Anthony Martin <github@martin-studio.com>
2015-11-25 16:46:08 -0800

    Confirmed, it's not a db driver issue.

--- test/workers/minecraft_server_log_monitor_test.rb
+++ test/workers/minecraft_server_log_monitor_test.rb
@@ -6,16 +6,6 @@ class MinecraftServerLogMonitorTest < ActiveSupport::TestCase
   end
   
   def test_perform
-    monitor = Thread.start do
-      5.times do
-        begin
-          MinecraftServerLogMonitor.perform(debug: true, "max_ticks" => 1)
-        rescue CobbleBotError => e
-          sleep 1
-          Preference.path_to_server = "#{Rails.root}/tmp"
-        end
-      end
-    end
-    skip 'Took too long to run.' unless monitor.join 5
+    MinecraftServerLogMonitor.perform(debug: true, "max_ticks" => 1)
   end
 end
