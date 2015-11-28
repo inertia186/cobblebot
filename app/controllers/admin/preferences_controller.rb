4c4160afb52123b9fe07208cb101d32c13d3b5a9
Anthony Martin <github@martin-studio.com>
2015-11-27 18:51:14 -0800

    refactored admin preferences

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -9,13 +9,13 @@ class Admin::PreferencesController < Admin::AdminController
     else
       Preference.find_or_create_all(false)
     end
-    
+
     respond_to do |format|
       format.html { }
       format.json { }
     end
   end
-  
+
   def edit_cell
     render 'edit_cell', layout: nil
   end
@@ -23,13 +23,13 @@ class Admin::PreferencesController < Admin::AdminController
   def slack_group_element
     render 'slack_group_element', layout: nil
   end
-  
+
   def update
     key = params[:id]
     @preference = Preference.find_or_create_by(key: key)
-    
+
     if key =~ /_json$/
-      val = params[:preference][:value]
+      val = preference_params[:value]
       line_no = 0
       begin
         val.each_line do |line|
@@ -38,10 +38,24 @@ class Admin::PreferencesController < Admin::AdminController
         end
       rescue JSON::ParserError => e
         @preference.value = val
-        @preference.errors[:value] << "Problem on line #{line_no}: #{e.message.split(': ').last}"
-        
-        render json: @preference.errors, status: :unprocessable_entity and return
+        @preference.errors[:value] << "has a problem on line #{line_no}: #{e.message.split(': ').last}"
       end
+    elsif key == 'path_to_server'
+      unless File.exists? preference_params[:value]
+        @preference.errors[:value] << 'does not exist.'
+      end
+    elsif key == 'irc_server_port'
+      val = preference_params[:value].to_i
+      if val.to_s != preference_params[:value]
+        @preference.errors[:value] << 'must be a valid integer.'
+      end
+      if val < 1 || val > 65535
+        @preference.errors[:value] << 'must be a valid port number (1 to 65535).'
+      end
+    end
+
+    if @preference.errors.any?
+      render json: @preference.errors, status: :unprocessable_entity and return
     end
 
     if @preference.update_attributes(preference_params)
