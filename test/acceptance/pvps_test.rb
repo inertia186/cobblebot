4f24a577e8f081edfa7f9dcbcfb179412c12625f
Anthony Martin <github@martin-studio.com>
2015-11-27 20:24:18 -0800

    Thanks, ajax.

--- test/acceptance/pvps_test.rb
+++ test/acceptance/pvps_test.rb
@@ -3,22 +3,24 @@ require "test_helper"
 class PvpsTest < ActionDispatch::IntegrationTest
   def setup
   end
-  
+
   def test_basic_workflow
     Server.mock_mode(up: true) do
       pvps = Message::Pvp.order("messages.created_at desc")
-      
+
       visit '/pvps'
-      
+
       css_path = 'count-up > span'
       result = page.evaluate_script("angular.element('#{css_path}').html();")
       assert_equal Message::Pvp.count, result.to_i
-      
+
       index = 0
-      
+
+      ajax_sync
+
       pvps.find_each do |pvp|
         nth = index = index + 1
-        
+
         css_path = "table > tbody > tr:nth-child(#{nth}) > td"
         result = page.evaluate_script("angular.element('#{css_path}').html();")
         refute_equal 'Searching ...', result
@@ -32,20 +34,20 @@ class PvpsTest < ActionDispatch::IntegrationTest
         result = page.evaluate_script("angular.element('#{css_path}').html();")
         assert_match pvp.author.nick, result
       end
-      
+
       fill_in 'query', with: 'resnullius'
       pvps = pvps.where(author: players(:resnullius))
 
       css_path = 'count-up > span'
       result = page.evaluate_script("angular.element('#{css_path}').html();")
       assert_equal pvps.count, result.to_i
-      
+
       css_path = "table > tbody > tr:nth-child(1) > td"
       result = page.evaluate_script("angular.element('#{css_path}').html();")
       refute_equal 'Dinnerbone was shot by Dinnerbone', result
     end
   end
-  
+
   def test_basic_json
     Server.mock_mode(up: true) do
       visit '/pvps.json'
